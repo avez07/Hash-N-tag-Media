a0a153e90f5587729bab225ac087a08c42ae0651
avez07 <ansariaavez12345@gmail.com>
2024-03-04 02:59:19 +0530

    finish

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -16,12 +16,25 @@ export default function Home() {
   const [folding_options, setFolding_options] = useState('');
   const [digital_proofs, setDigital_proofs] = useState('');
   const [job_samples, setJob_samples] = useState('');
+  const [rates, setRates] = useState('0.179');
+  const [marketprice, setMarketPrice] = useState(0);
+
+
+
   const [titleOptions, setTitileOption] = useState([])
+  const [sizeOptions, setSizeOption] = useState([])
+  const [stockOptions, setStockOption] = useState([])
+  const [colorspecOptions, setColorspecOption] = useState([])
+  const [coatingOptions, setCoatingOption] = useState([])
+  const [folding_optionsOptions, setFolding_optionsOption] = useState([])
+  const [digital_proofsOptions, setDigital_proofsOption] = useState([])
+  const [job_samplesOptions, setJob_samplesOption] = useState([])
+
 
   useEffect(() => {
-    getTitle()
     getData();
-  }, [title])
+    console.log(sizeOptions);
+  }, [title, size, stock, colorspec, coating, folding_options, digital_proofs, job_samples])
 
   const getTitle = async () => {
     const requestOptions = {
@@ -38,14 +51,14 @@ export default function Home() {
     myHeaders.append("Content-Type", "application/json");
 
     const raw = JSON.stringify({
-      title : title,
-      size : size,
-      stock:stock,
-      colorspec:colorspec,
-      coating:coating,
-      folding_options:folding_options,
-      digital_proofs:digital_proofs,
-      job_samples:job_samples
+      title: title,
+      size: size,
+      stock: stock,
+      colorspec: colorspec,
+      coating: coating,
+      folding_options: folding_options,
+      digital_proofs: digital_proofs,
+      job_samples: job_samples
     });
     const requestOptions = {
       method: "POST",
@@ -54,28 +67,80 @@ export default function Home() {
       redirect: "follow"
     };
     fetch("http://localhost:8000/getData", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(result))
+      .then((response) => response.json())
+      .then((result) => {
+        if (result[0].rate) {
+          setRates(result[0].rate);
+        }
+        switch (true) {
+          case result[0].hasOwnProperty('title'):
+            setTitileOption(result.map((items) => items.title))
+            break;
+          case result[0].hasOwnProperty('size'):
+            setSizeOption(result.map((items) => items.size))
+            break;
+          case result[0].hasOwnProperty('stock'):
+            setStockOption(result.map((items) => items.stock))
+            break;
+          case result[0].hasOwnProperty('colorspec'):
+            setColorspecOption(result.map((items) => items.colorspec))
+            break;
+          case result[0].hasOwnProperty('coating'):
+            setCoatingOption(result.map((items) => items.coating))
+            break;
+          case result[0].hasOwnProperty('folding_options'):
+            setFolding_optionsOption(result.map((items) => items.folding_options))
+            break;
+
+          case result[0].hasOwnProperty('digital_proofs'):
+            setDigital_proofsOption(result.map((items) => items.digital_proofs))
+            break;
+          case result[0].hasOwnProperty('job_samples'):
+            setJob_samplesOption(result.map((items) => items.job_samples))
+            break;
+
+          default:
+            break;
+        }
+      })
       .catch((error) => console.error(error));
   }
   const handleSelectChange = (event) => {
-    const { name, value } = event.target;  
+    const { name, value } = event.target;
     switch (name) {
       case "title":
-        setTitile(value);
+        setTitile(value); setSize(''); setStock(''); setColorspec(''); setCoating(''); setFolding_options(''); setDigital_proofs(''); setJob_samples('')
         break;
       case "size":
-        setSize(value);
+        setSize(value); setStock(''); setColorspec(''); setCoating(''); setFolding_options(''); setDigital_proofs(''); setJob_samples('');
         break;
       case "stock":
-        setStock(value);
+        setStock(value); setColorspec(''); setCoating(''); setFolding_options(''); setDigital_proofs(''); setJob_samples('');
         break;
+      case "Colorspec":
+        setColorspec(value); setCoating(''); setFolding_options(''); setDigital_proofs(''); setJob_samples('');
+        break;
+      case "Coating":
+        setCoating(value); setFolding_options(''); setDigital_proofs(''); setJob_samples('');
+        break;
+      case "Folding_options":
+        setFolding_options(value); setDigital_proofs(''); setJob_samples('');
+        break;
+      case "Digital_proofs":
+        setDigital_proofs(value); setJob_samples('');
+        break;
+      case "Job_samples":
+        setJob_samples(value);
+        break;
+        case "Mp":
+          setMarketPrice(value)
       // Add cases for other select fields as needed
       default:
         break;
     }
+    // getData();
   };
- 
+
   return (
     <>
       <Container className="mt-3">
@@ -99,16 +164,115 @@ export default function Home() {
               <div className="d-flex justify-content-betwwen align-items-center mt-4">
                 <label className="fw-semibold" style={{ width: '30%' }}>Title<span className="text-danger">*</span></label>
                 <Form.Select onChange={handleSelectChange} name="title" value={title} style={{ width: '70%' }} aria-label="Default select example">
-                  <option>Open this select menu</option>
+                  <option>Stlect tittle</option>
                   {Array.isArray(titleOptions) && titleOptions.map((item, index) => (
                     <option key={index} value={item}>
                       {item}
                     </option>
                   ))}
-
+                </Form.Select>
+              </div>
+              <div className="d-flex justify-content-betwwen align-items-center mt-4">
+                <label className="fw-semibold" style={{ width: '30%' }}>Size<span className="text-danger">*</span></label>
+                <Form.Select onChange={handleSelectChange} disabled={!title ? true : false} name="size" value={size} style={{ width: '70%' }} aria-label="Default select example">
+                  <option>Select size</option>
+                  {Array.isArray(sizeOptions) && sizeOptions.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </Form.Select>
+              </div>
+              <div className="d-flex justify-content-betwwen align-items-center mt-4">
+                <label className="fw-semibold" style={{ width: '30%' }}>Stock<span className="text-danger">*</span></label>
+                <Form.Select onChange={handleSelectChange} disabled={!size ? true : false} name="stock" value={stock} style={{ width: '70%' }} aria-label="Default select example">
+                  <option>Slect Stock</option>
+                  {Array.isArray(stockOptions) && stockOptions.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </Form.Select>
+              </div>
+              <div className="d-flex justify-content-betwwen align-items-center mt-4">
+                <label className="fw-semibold" style={{ width: '30%' }}>Colorspec<span className="text-danger">*</span></label>
+                <Form.Select onChange={handleSelectChange} disabled={!stock ? true : false} name="Colorspec" value={colorspec} style={{ width: '70%' }} aria-label="Default select example">
+                  <option>Select Colorspec</option>
+                  {Array.isArray(colorspecOptions) && colorspecOptions.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </Form.Select>
+              </div>
+              <div className="d-flex justify-content-betwwen align-items-center mt-4">
+                <label className="fw-semibold" style={{ width: '30%' }}>Coating<span className="text-danger">*</span></label>
+                <Form.Select onChange={handleSelectChange} disabled={!colorspec ? true : false} name="Coating" value={coating} style={{ width: '70%' }} aria-label="Default select example">
+                  <option>Select Coation</option>
+                  {Array.isArray(coatingOptions) && coatingOptions.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </Form.Select>
+              </div>
+              <div className="d-flex justify-content-betwwen align-items-center mt-4">
+                <label className="fw-semibold" style={{ width: '30%' }}>Folding Options<span className="text-danger">*</span></label>
+                <Form.Select onChange={handleSelectChange} disabled={!coating ? true : false} name="Folding_options" value={folding_options} style={{ width: '70%' }} aria-label="Default select example">
+                  <option>Select Folding Option</option>
+                  {Array.isArray(folding_optionsOptions) && folding_optionsOptions.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))}
                 </Form.Select>
               </div>
 
+              <div className="d-flex justify-content-betwwen align-items-center mt-4">
+                <label className="fw-semibold" style={{ width: '30%' }}>Digital Prove<span className="text-danger">*</span></label>
+                <Form.Select onChange={handleSelectChange} disabled={!folding_options ? true : false} name="Digital_proofs" value={digital_proofs} style={{ width: '70%' }} aria-label="Default select example">
+                  <option>Select Digital Prove</option>
+                  {Array.isArray(digital_proofsOptions) && digital_proofsOptions.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </Form.Select>
+              </div>
+              <div className="d-flex justify-content-betwwen align-items-center mt-4">
+                <label className="fw-semibold" style={{ width: '30%' }}>Job Sample<span className="text-danger">*</span></label>
+                <Form.Select onChange={handleSelectChange} disabled={!digital_proofs ? true : false} name="Job_samples" value={job_samples} style={{ width: '70%' }} aria-label="Default select example">
+                  <option>Select Job Sample</option>
+                  {Array.isArray(job_samplesOptions) && job_samplesOptions.map((item, index) => (
+                    <option key={index} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </Form.Select>
+              </div>
+            </div>
+
+            <div>
+              <table id="My-table"className="mt-4 text-center">
+                <thead>
+                  <tr>
+                    <th>Run</th>
+                    <th>Unit</th>
+                    <th>Total</th>
+                    <th>Market Price <input name="Mp"  value={marketprice} onChange={handleSelectChange}/>%</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Array.from({ length: 5 }, (_, index) => (
+                    <tr key={index}>
+                      <td>{100 + index * 100}</td>
+                      <td>{rates}</td>
+                      <td>{((100 + index * 100) * rates).toFixed(3)}</td>
+                      <td>{(((100 + index * 100) * rates)* (1 + marketprice / 100)).toFixed(3)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </Col>
         </Row>
--- server/index.js
+++ server/index.js
@@ -7,34 +7,38 @@ app.use(express.json())
 app.use(cors())
 app.get('/title', async (req, res) => {
     try {
-   const [rows] = await db.query('SELECT title FROM flyers_brochures  GROUP BY title')
+   const [rows] = await db.query('SELECT title FROM `flyers_brochures`  GROUP BY title')
    const titles = rows.map(item => item.title);
    res.json(titles)
     } catch (error) {
         console.log(error)
     }
 });
-app.post('/getData', (req, res) => {
+app.post('/getData', async (req, res) => {
    const data = req.body;
    try {
     let query  = 'SELECT ';
-    let whereClause = "WHERE "
+    let whereClause = " WHERE 1 "
     let groupByClause = '';
     for (const [key, value] of Object.entries(data)) {
         if (value !== '') {
-          query += `${key}, `;
-          whereClause += ` ${key} = '${value}' AND `;
+        //   query += `${key}, `;
+          whereClause += `AND ${key} = '${value}'  `;
         } else {
             query += `${key}, `;
           groupByClause = key;
           break;
         }
     }
-        query = query.slice(0, -2); // Remove the last comma
-        whereClause = whereClause.slice(0, -5); // Remove the last ' AND 
-        query += ` FROM flyers_brochures ${whereClause !== ' WHERE ' ? whereClause : ''} GROUP BY ${groupByClause}`;
-        const [rows] = db.query(query);
-        res.json(rows)
+        query = query.slice(0, -2);
+        // whereClause = whereClause.slice(0, -5);
+        query += ` ,rate FROM flyers_brochures ${whereClause !== ' WHERE ' ? whereClause : ''} GROUP BY ${groupByClause}`;
+        const [rows] = await db.query(query).catch(error => {
+            console.error("Error executing query:", error);
+            throw new Error("Error executing query");
+        });;
+        res.status(200).json(rows)
+        
     
    } catch (error) {
     console.log(error)
